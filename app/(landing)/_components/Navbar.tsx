85023a2c76836ae4ede8d998ec5737e90c634b8c
VIKRANT SHARMA <vikrant500sharma@gmail.com>
2024-03-02 20:10:08 +0530

    commit

--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -26,12 +26,7 @@ export const Navbar = () => {
              Join your Organization
             </Link>
           </Button>
-          <Button size="sm" asChild id="home-button">
-            <Link href="/home">
-            Home
-            </Link>
-          </Button>
-          
+       
           
          
         </div>
