a26d839b28d1be807c0f53f0e308a934a3d5cb0e
Turbobot <turbobot@vercel.com>
2024-04-08 21:42:00 +0530

    feat(create-turbo): apply official-starter transform

--- app/(auth)/organizations/organization-portal.tsx/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OrganizationList } from "@clerk/nextjs";
-export default function OrganizationListPage() {
-    return (
-      <OrganizationList
-          hidePersonal={true}
-       
-      />
-    );
-  };
--- app/(dashboard)/u/[username]/(home)/page.tsx
+++
@@ -1,9 +0,0 @@
-const CreatorPage = () => {
-    return ( 
-      <div>
-        Creator Page
-      </div>
-    );
-  }
-  
-  export default CreatorPage;
